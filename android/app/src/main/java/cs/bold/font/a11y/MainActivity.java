ca0d386f671abe22218511e63b3264099bb9e344
damian.ionic <damian@ionic.io>
2024-06-10 12:41:32 -0700

    initial

---
+++ android/app/src/main/java/cs/bold/font/a11y/MainActivity.java
@@ -0,0 +1,5 @@
+package cs.bold.font.a11y;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.bold.font.a11y',
+  appName: 'cs-bold-font-a11y',
+  webDir: 'www'
+};
+
+export default config;
